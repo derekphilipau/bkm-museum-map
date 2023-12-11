0c8e3e49f51cfb1f3848d65d5506f59b82c3b2d8
Derek Philip Au <22045002+derekphilipau@users.noreply.github.com>
2023-12-11 17:31:12 -0500

    add selectors, simplify svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,39 @@
-import { MuseumMap } from '@/components/museum-map';
+'use client';
+
+import { useState } from 'react';
+
+import { MuseumMap, galleries } from '@/components/museum-map';
 
 export default function Page() {
-  const name = 'Arts of the Islamic World, 2nd floor';
+  const [name, setName] = useState('Arts of the Islamic World, 2nd floor');
+
+  const handleGalleryClick = (galleryName) => {
+    setName(galleryName);
+  };
+
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
+    <section className="container grid grid-cols-1 gap-6 py-8 sm:grid-cols-2">
+      <div className="">
         <div className="h-96">
           <MuseumMap locationName={name} />
         </div>
-        <p className="max-w-[700px] text-lg text-neutral-700 dark:text-neutral-400 sm:text-xl"></p>
+        <p className="text-lg text-neutral-700 dark:text-neutral-400 sm:text-xl">
+          {name}
+        </p>
       </div>
-      <div className="flex gap-4"></div>
+      <div className="flex gap-4">
+        <ul>
+          {galleries.map((gallery) => (
+            <li
+              key={gallery.name}
+              className="cursor-pointer text-sm text-blue-500 hover:text-blue-700 dark:hover:text-blue-300"
+              onClick={() => handleGalleryClick(gallery.name)}
+            >
+              {gallery.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
